a071f15ad70cfa40386546ad3eceab5d13d9ddd3
anarkisoft <anarkisoft@gmail.com>
2024-02-02 19:28:27 -0500

    add sheet style

--- index.js
+++ index.js
@@ -6,20 +6,31 @@ let FULL_URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?sheet
 
 // Agregar una función de manejo de respuesta para JSONP
 window.handleResponse = function(response) {
+    if (!response.table || !response.table.cols || !response.table.rows) {
+        console.error('La respuesta de la API no tiene el formato esperado.');
+        return;
+    }
+
     const columnTitles = response.table.cols.map(col => col.label); // Obtener los títulos de las columnas
 
     const data = response.table.rows.map(row => {
-      const rowData = {};
-      row.c.forEach((cell, index) => {
-        const colTitle = columnTitles[index];
-        rowData[colTitle] = cell.v;
-      });
-      return rowData;
+        const rowData = {};
+        if (row.c) {
+            row.c.forEach((cell, index) => {
+                const colTitle = columnTitles[index];
+                rowData[colTitle] = cell && cell.v ? cell.v : null;
+            });
+        }
+        return rowData;
     });
+
+    // Mostrar el nombre en el elemento HTML
     const nombreElement = document.getElementById('nombrePlaceholder');
-    nombreElement.textContent = data[0]['Nombre'] || 'juancho';
-  
-    console.log(data);
+    nombreElement.textContent = data[0] && data[0]['Nombre'] ? data[0]['Nombre'] : 'juancho'; 
+
+    // Cambiar la propiedad src de la imagen
+    const imagenElement = document.getElementById('imagenPlaceholder');
+    imagenElement.src = data[0] && data[0]['Imagen'] ? data[0]['Imagen'] : 'assets/img/service/services1.jpg';
 };
 
 // Crear un script dinámicamente para hacer la solicitud JSONP
