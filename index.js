684f07378d88f594af83027a42db39f856e796c9
brandon estiven moreno rubiano <brandonli777xd@gmail.com>
2024-02-12 21:30:45 -0500

    ultimos cambios 12/02/2024

--- index.js
+++ index.js
@@ -37,3 +37,15 @@ window.handleResponse = function(response) {
 const script = document.createElement('script');
 script.src = FULL_URL;
 document.body.appendChild(script);
+
+
+
+const slider = document.querySelector(".slider");
+
+function activate(e) {
+  const items = document.querySelectorAll(".item");
+  e.target.matches(".next") && slider.append(items[0]);
+  e.target.matches(".prev") && slider.prepend(items[items.length - 1]);
+}
+
+document.addEventListener("click", activate, false);
